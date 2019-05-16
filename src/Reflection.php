8c8dbcd230c9a6375b9cb8a499567e91a3ef5cab
Vesko Kenashkov <vesko@azonmedia.com>
2019-05-16 15:26:58 +0300

    - initial

---
+++ src/Reflection.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+
+namespace Azonmedia\Reflection;
+
+/**
+ * Class Reflection
+ * @package Reflection
+ *
+ */
+class Reflection extends \Reflection
+{
+
+}
---
+++ src/ReflectionClass.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+
+namespace Azonmedia\Reflection;
+
+/**
+ * Class ReflectionClass
+ * @package Reflection
+ *
+ */
+class ReflectionClass extends \ReflectionClass
+{
+
+}
