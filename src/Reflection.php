17ec2bd709b68bd7a026935c0c0c7f86c3d4f806
Vesko Kenashkov <vesko@azonmedia.com>
2019-05-16 16:03:53 +0300

    - added classes

--- src/Reflection.php
+++ src/Reflection.php
@@ -12,4 +12,44 @@ namespace Azonmedia\Reflection;
 class Reflection extends \Reflection
 {
 
+
+    /**
+     * Returns a ReflectionFunction based on the provided callable.
+     * For example for a::b it will return a ReflectionMethod, while 'asd', will return ReflectionFunction
+     *
+     * This method is needed because the PHP callable supports many formats while
+     *
+     * @param callable $callable
+     * @return \ReflectionFunctionAbstract
+     *
+     * @author vesko@azonmedia.com
+     * @created 5.10.2019
+     */
+    public static function getFromCallable(callable $callable) : \ReflectionFunctionAbstract
+    {
+        if (is_string($callable)) {
+            //this may be:
+            //function - 'functionname'
+            //static method call - 'classname::methodname'
+            if (strpos($callable, '::') >= 1) {
+                [$class, $method] = explode('::', $callable, 2);
+                $ret = new \ReflectionMethod($class, $method);
+            } else {
+                //no need to do a function_exists call as the callable type to the $callable parameter already checks this
+                $ret = new \ReflectionFunction($callable);
+            }
+        } elseif(is_array($callable)) {
+            //this may be
+            //dynamic call - [$instnace, 'methodname']
+            //static call - ['classname', 'methodname']
+            //in both cases the below should be OK
+            $ret = new \ReflectionMethod($callable[0], $callable[1]);
+        } elseif($callable instanceof \Closure) {
+            $ret = new \ReflectionFunction($callable);
+        } else {
+            //this is an object with __invoke defined
+            $ret = new \ReflectionMethod($callable, '__invoke');
+        }
+        return $ret;
+    }
 }
--- src/ReflectionClass.php
+++ src/ReflectionClass.php
@@ -11,5 +11,60 @@ namespace Azonmedia\Reflection;
  */
 class ReflectionClass extends \ReflectionClass
 {
+    /**
+     * @return array
+     */
+    public function getParentClasses() : array
+    {
+        $stack = array();
+        $parent_class = $this;
+        while ($parent_class = $parent_class->getParentClass()) {
+            $stack[] = $parent_class->name;
+        }
+        return $stack;
+    }
 
+    /**
+     * Returns bool does this class extend the provided $class
+     * @param string $class
+     * @return bool
+     */
+    public function extendsClass(string $class) : bool
+    {
+        return in_array($class,$this->getParentClasses());
+    }
+
+    /**
+     * Returns the inheritance distance between this class and the provided $class (which should be a parent class)
+     * @param string $class
+     * @return int
+     */
+    public function extendsClassDistance(string $class) : int
+    {
+        $pos = array_search($class, $this->getParentClasses());
+        if ($pos===false) {
+            return $pos;
+        } else {
+            return $pos+1;//0 position means 1 distance
+        }
+    }
+
+    /**
+     * Checks does the class define the provided methods as its own (not through inheritance)
+     * @param string $method Method name
+     * @return bool
+     */
+    public function hasOwnMethod(string $method) : bool
+    {
+        $ret = FALSE;
+        try {
+            $Rmethod = $this->getMethod($method);
+            if ($Rmethod->name == $method && $Rmethod->class == $this->name) {
+                $ret = TRUE;
+            }
+        } catch (\ReflectionException $exception) {
+
+        }
+        return $ret;
+    }
 }
